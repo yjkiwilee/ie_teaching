0aa00267677f34edd3654816cd0db378c86a9581
Young Jun Lee <youngjun.lee@reuben.ox.ac.uk>
2025-10-17 14:57:59 +0100

    Edit trial tests

--- week_1/testing/inflammation/tests/test_trial.py
+++ week_1/testing/inflammation/tests/test_trial.py
@@ -19,7 +19,7 @@ class TestTrial:
         test_result = np.array([0, 0])
 
         # Need to use Numpy testing functions to compare arrays
-        npt.assert_array_equal(trial_1.data_mean(), test_result)
+        npt.assert_array_equal(trial_1.daily_mean(), test_result)
 
 
     def test_daily_mean_integers(self, trial_1):
@@ -31,7 +31,7 @@ class TestTrial:
         test_result = np.array([3, 4])
 
         # Need to use Numpy testing functions to compare arrays
-        npt.assert_array_equal(trial_1.data_mean(), test_result)
+        npt.assert_array_equal(trial_1.daily_mean(), test_result)
 
     def test_daily_max_integers(self, trial_1):
         """Test that the daily max functions works for an array of positive integers."""
@@ -40,7 +40,7 @@ class TestTrial:
         test_result = np.array([8, 7, 9])
 
         # Need to use Numpy testing functions to compare arrays
-        npt.assert_array_equal(trial_1.data_max(), test_result)
+        npt.assert_array_equal(trial_1.daily_max(), test_result)
     
     def test_daily_min_integers(self, trial_1):
         """Test that the daily max functions works for an array of positive integers."""
@@ -49,7 +49,7 @@ class TestTrial:
         test_result = np.array([2, 6, 1, 1])
 
         # Need to use Numpy testing functions to compare arrays
-        npt.assert_array_equal(trial_1.data_min(), test_result)
+        npt.assert_array_equal(trial_1.daily_min(), test_result)
 
     def test_daily_min_string(self, trial_1):
         """Test for TypeError when passing strings"""
@@ -59,10 +59,37 @@ class TestTrial:
         with pytest.raises(TypeError):
             trial_1.data(min())
 
-    def test_patient_normalise(self, trial_1):
+    @pytest.mark.parametrize(
+        "test, expected, expect_raises",
+        [
+            (
+                np.array([[-1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                None,
+                ValueError,
+            ),
+            (
+                np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                np.array([[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
+                None,
+            ),
+            (
+                'Foo',
+                None,
+                TypeError
+            ),
+            (
+                np.array([1, 2, 3, 4]),
+                None,
+                ValueError,
+            ),
+        ])
+    def test_patient_normalise(self, test, expected, expect_raises, trial_1):
         """Test normalisation works for arrays of one and positive integers."""
 
-        trial_1.data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        test_result = np.array([[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+        trial_1.data = test
 
-        npt.assert_almost_equal(trial_1.patient_normalise(), test_result, decimal=2)
+        if expect_raises is not None:
+            with pytest.raises(expect_raises):
+                npt.assert_almost_equal(trial_1.patient_normalise(), np.array(expected), decimal=2)
+        else:
+            npt.assert_almost_equal(trial_1.patient_normalise(), np.array(expected), decimal=2)
